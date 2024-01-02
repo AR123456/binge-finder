06f1b77e2164ef54548575438477abe24ea97e92
AR123456 <coloradoz99@gmail.com>
2024-01-02 07:04:38 -0700

    locations array

--- Practice Projects/dragon-repeller/script.js
+++ Practice Projects/dragon-repeller/script.js
@@ -1,3 +1,4 @@
+//variables
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -5,7 +6,25 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Buy 10 health (10 gold)", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+];
+// selecting elemenst
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -28,7 +47,8 @@ function goTown() {
   button1.onclick = goStore;
   button2.onclick = goCave;
   button3.onclick = fightDragon;
-  text.innerText = "You enter the store";
+  text.innerText =
+    'You are in the town square. You see a sign that says "Store" ';
 }
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
@@ -37,9 +57,9 @@ function goStore() {
   button1.onclick = buyHealth;
   button2.onclick = buyWeapon;
   button3.onclick = goTown;
-  text.innerText =
-    "You are in the town square. You see a sign that says Store.";
+  text.innerText = "You enter the store";
 }
+function update(location) {}
 function buyHealth() {}
 function buyWeapon() {}
 
