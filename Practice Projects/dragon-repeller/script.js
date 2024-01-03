1fd682b31ef4e30ea8f9a935dec09a541267b216
AR123456 <coloradoz99@gmail.com>
2024-01-03 06:42:31 -0700

    buying weapons

--- Practice Projects/dragon-repeller/script.js
+++ Practice Projects/dragon-repeller/script.js
@@ -6,6 +6,12 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+const weapons = [
+  { name: "stick", power: 5 },
+  { name: "dagger", power: 30 },
+  { name: "claw hammer", power: 50 },
+  { name: "sword", power: 100 },
+];
 const locations = [
   {
     name: "town square",
@@ -71,7 +77,25 @@ function buyHealth() {
     text.innerText = "You do not have enough gold to buy health.";
   }
 }
-function buyWeapon() {}
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold.";
+    button2.onclick = sellWeapon;
+  }
+}
 
 function goCave() {
   update(locations[2]);
