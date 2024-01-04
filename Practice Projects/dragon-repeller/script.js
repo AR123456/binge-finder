6c1accf77b471e1871d3acb2f7a10bfdc0e06f6e
AR123456 <coloradoz99@gmail.com>
2024-01-04 07:50:17 -0700

    attack function

--- Practice Projects/dragon-repeller/script.js
+++ Practice Projects/dragon-repeller/script.js
@@ -126,19 +126,33 @@ function fightSlime() {
 }
 function fightBeast() {
   fighting = 1;
+  goFight();
 }
 
 function fightDragon() {
   fighting = 2;
+  goFight();
 }
 
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function goCave() {
   update(locations[2]);
 }
-function attack() {}
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -=
+    weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+}
 function dodge() {}
