bddaee94bf7e629b49be4648dcacaf2810f6b6e0
AR123456 <coloradoz99@gmail.com>
2024-01-08 06:25:25 -0700

    current weapon dec if breakage

--- Practice Projects/dragon-repeller/script.js
+++ Practice Projects/dragon-repeller/script.js
@@ -205,11 +205,18 @@ function attack() {
   } else if (monsterHealth <= 0) {
     fighting === 2 ? winGame() : defeatMonster();
   }
+  if (Math.random() <= 0.1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
+  }
 }
 function getMonsterAttackValue(level) {
   const hit = level * 5 - Math.floor(Math.random() * xp);
   console.log(hit);
   return hit > 0 ? hit : 0;
+}
+function isMonsterHit() {
+  return Math.random() > 0.2 || health < 20;
 }
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
