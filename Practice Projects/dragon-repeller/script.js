414e23ca24bd25371294750f2db52204324c1754
AR123456 <coloradoz99@gmail.com>
2024-01-08 06:26:05 -0700

    inventory pop

--- Practice Projects/dragon-repeller/script.js
+++ Practice Projects/dragon-repeller/script.js
@@ -205,6 +205,9 @@ function attack() {
   } else if (monsterHealth <= 0) {
     fighting === 2 ? winGame() : defeatMonster();
   }
+  if (Math.random() <= 0.1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+  }
 }
 function getMonsterAttackValue(level) {
   const hit = level * 5 - Math.floor(Math.random() * xp);
