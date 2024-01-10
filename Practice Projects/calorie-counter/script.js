d74acabbe5ea416d851ebc96ca4f374aab560dae
AR123456 <coloradoz99@gmail.com>
2024-01-10 06:36:38 -0700

    stuck on regex

--- Practice Projects/calorie-counter/script.js
+++ Practice Projects/calorie-counter/script.js
@@ -10,12 +10,18 @@ const output = document.getElementById("output");
 let isError = false;
 
 const cleanInputString = (str) => {
-  const strArray = str.split("");
-  const cleanStrArray = [];
-  for (let i = 0; i < strArray.length; i++) {
-    if (!["+", "-", " "].includes(strArray[i])) {
-      cleanStrArray.push(strArray[i]);
-    }
-  }
+  //   const strArray = str.split("");
+  //   const cleanStrArray = [];
+  //   for (let i = 0; i < strArray.length; i++) {
+  //     if (!["+", "-", " "].includes(strArray[i])) {
+  //       cleanStrArray.push(strArray[i]);
+  //     }
+  //   }
+  // changing over to regex - checking for + - and whitespace character and continue looking after
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+};
+const isInvalidInput = (str) => {
+  const regex = /e[0-9]e/i;
 };
 cleanInputString();
