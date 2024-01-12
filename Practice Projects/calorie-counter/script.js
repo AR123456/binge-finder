257bf6b1dbae256da48f5f0b2a85748d09f13913
AR123456 <coloradoz99@gmail.com>
2024-01-12 03:36:26 -0700

    return calories

--- Practice Projects/calorie-counter/script.js
+++ Practice Projects/calorie-counter/script.js
@@ -50,7 +50,12 @@ const getCaloriesFromInputs = (list) => {
     const currVal = cleanInputString(list[i].value);
     const invalidInputMatch = isInvalidInput(currVal);
     if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
     }
+    calories += Number(currVal);
   }
+  return calories;
 };
 addEntryButton.addEventListener("click", addEntry);
