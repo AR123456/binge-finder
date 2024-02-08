1acb5295545badb0782838ed1a53e02334cb254b
AR123456 <coloradoz99@gmail.com>
2024-02-08 07:17:40 -0700

    looking at the ternary issue

--- Practice Projects/music-player/script.js
+++ Practice Projects/music-player/script.js
@@ -168,6 +168,13 @@ const renderSongs = (array) => {
     .join("");
   playlistSongs.innerHTML = songsHTML;
 };
+const setPlayButtonAccessibleText = () => {
+  // This function will set the aria-label attribute to the current song, or to the first song in the playlist. And if the playlist is empty, it sets the aria-label to "Play".
+  // get first song or currently playing
+  const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute("aria-label", `Play ${song.title}`);
+};
+
 // need to play next and previous song
 const getCurrentSongIndex = () => {
   // index of the current song
