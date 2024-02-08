dae469be9de427d923afa795ecb18af61bb0888b
AR123456 <coloradoz99@gmail.com>
2024-02-08 07:16:51 -0700

    assign current title and current artist

--- Practice Projects/music-player/script.js
+++ Practice Projects/music-player/script.js
@@ -128,6 +128,13 @@ const playPreviousSong = () => {
     playSong(previousSong.id);
   }
 };
+// display current song title and artist
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+};
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
