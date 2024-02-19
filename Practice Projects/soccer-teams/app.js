227dfaaab931bf9bb2872bd6919dc863d2acaacb
AR123456 <coloradoz99@gmail.com>
2024-02-19 07:11:28 -0500

    set player cards

--- Practice Projects/soccer-teams/app.js
+++ Practice Projects/soccer-teams/app.js
@@ -184,3 +184,7 @@ const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
 typeOfSport.textContent = sport;
 teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+// display from drop down menu
+const setPlayerCards = () => {};
