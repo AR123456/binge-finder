8dad4770305813ad54f9b395a37f2e878accd0dd
AR123456 <coloradoz99@gmail.com>
2024-02-21 08:24:53 -0500

    nick name

--- Practice Projects/soccer-teams/app.js
+++ Practice Projects/soccer-teams/app.js
@@ -194,6 +194,7 @@ const setPlayerCards = (arr = players) => {
       <h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
       <p>Position: ${position}</p>
       <p>Number: ${number}</p>
+      <p>Nickname: </p>
       </div>`;
     }
   );
