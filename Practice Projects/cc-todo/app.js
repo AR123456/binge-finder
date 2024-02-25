157c87125f25a580d00c6f6f7c3f9442dfc39251
AR123456 <coloradoz99@gmail.com>
2024-02-25 06:17:58 -0700

    declare vars

---
+++ Practice Projects/cc-todo/app.js
@@ -0,0 +1,6 @@
+// vars
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+
+// local storage key value pairs
