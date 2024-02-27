3de9b061c4472d09e7919c4f9a6f192ba6d15322
AR123456 <coloradoz99@gmail.com>
2024-02-27 05:53:15 -0700

    writing date to page

--- Practice Projects/date-formatter/script.js
+++ Practice Projects/date-formatter/script.js
@@ -5,3 +5,8 @@ const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const formattedDate = `${day}-${month}-${year}`;
+
+currentDateParagraph.textContent = formattedDate;
