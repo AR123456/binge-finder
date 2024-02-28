d1be3e34638b33945c013d82bdb109dc0035b491
AR123456 <coloradoz99@gmail.com>
2024-02-28 05:57:45 -0700

    js and input

--- Certification Projects/Check-Palindrome/app.js
+++ Certification Projects/Check-Palindrome/app.js
@@ -7,15 +7,13 @@ const checkForPalindrome = (e) => {
     alert("Please input a value");
   } else {
     // remove any non alphabet characters from the input
-    cleanInput = input.value
-      .trim()
-      .toLowerCase()
-      .replace(/[^a-z]/g, "");
+    cleanInput = input.value.toLowerCase().replace(/[^a-z]/g, "");
 
     if (cleanInput === "a") {
-      result.innerHTML = "A is a palindrome";
+      console.log("A is a palindrome");
     } else {
-      //
+      //  Code Camp palindrome checker   https://www.freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
+      // check for palindrome
     }
   }
 };
