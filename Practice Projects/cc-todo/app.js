2013c630105622fb68d897a65408886e4211cb61
AR123456 <coloradoz99@gmail.com>
2024-03-01 06:35:16 -0700

    formatting the task object

--- Practice Projects/cc-todo/app.js
+++ Practice Projects/cc-todo/app.js
@@ -34,3 +34,6 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+const taskObj = {
+  id: `${titleInput.value.toLowerCase().split(" ").join("-")}`,
+};
