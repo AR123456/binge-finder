ae63075f0abf7bc49546118784f697977b7f08bf
AR123456 <coloradoz99@gmail.com>
2024-03-01 06:34:26 -0700

    close and discard

--- Practice Projects/cc-todo/app.js
+++ Practice Projects/cc-todo/app.js
@@ -26,3 +26,7 @@ closeTaskFormBtn.addEventListener("click", () => {
 cancelBtn.addEventListener("click", () => {
   confirmCloseDialog.close();
 });
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
