c8a8d892a33a0a4bfabfecc84b9bb6c344b41084
AR123456 <coloradoz99@gmail.com>
2024-03-05 07:00:50 -0700

    current task

--- Practice Projects/cc-todo/app.js
+++ Practice Projects/cc-todo/app.js
@@ -60,6 +60,11 @@ const editTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+  const currentTask = taskData[dataArrIndex];
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+  addOrUpdateTaskBtn.innerText = "Update Task";
 };
 // clear the add task form after adding
 const reset = () => {
