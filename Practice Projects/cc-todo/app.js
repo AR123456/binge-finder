939c9a80fbb16270816e0e5a8520daa7c9097c70
AR123456 <coloradoz99@gmail.com>
2024-03-05 06:58:10 -0700

    hide form show task

--- Practice Projects/cc-todo/app.js
+++ Practice Projects/cc-todo/app.js
@@ -45,8 +45,12 @@ taskForm.addEventListener("submit", (e) => {
     tasksContainer.innerHTML += `
 <div class="task" id="${id}">
 <p><strong>Title: </strong>${title}</p>
+<p><strong>Date: </strong>${date}</p>
+<p><strong>Description: </strong>${description}</p>
+<button type="button" class="btn">Edit</button>
+<button type="button" class="btn">Delete</button>
 </div>
-    
-    `;
+        `;
   });
+  taskForm.classList.toggle("hidden");
 });
