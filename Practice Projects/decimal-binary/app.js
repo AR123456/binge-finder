e58daa008c6d49b0a4f251f5f9fabdfc38960acb
AR123456 <coloradoz99@gmail.com>
2024-03-09 07:52:21 -0700

    empty string is falsey

--- Practice Projects/decimal-binary/app.js
+++ Practice Projects/decimal-binary/app.js
@@ -3,10 +3,15 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
+  // checking for empty string which is faulsy
+  if (!numberInput.value) {
+  }
   console.log(numberInput.value);
 };
 
 convertBtn.addEventListener("click", checkUserInput);
 numberInput.addEventListener("keydown", (e) => {
-  console.log(e);
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
 });
