b16efa8dea846fddc70785f5088712164d4f30e4
AR123456 <coloradoz99@gmail.com>
2024-03-09 07:52:43 -0700

    alert if input is not a number

--- Practice Projects/decimal-binary/app.js
+++ Practice Projects/decimal-binary/app.js
@@ -3,8 +3,9 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-  // checking for empty string which is faulsy
-  if (!numberInput.value) {
+  // checking for empty string which is faulsy, or not an integer with parseInt wrapped in isNaN
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
   }
   console.log(numberInput.value);
 };
