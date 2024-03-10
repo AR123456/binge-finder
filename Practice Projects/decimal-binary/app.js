69fd32968e9a9d772a3c090ffb9f7f48318ec1f7
AR123456 <coloradoz99@gmail.com>
2024-03-10 14:58:50 -0600

    checking for 0

--- Practice Projects/decimal-binary/app.js
+++ Practice Projects/decimal-binary/app.js
@@ -6,11 +6,23 @@ const decimalToBinary = (input) => {
   const inputs = [];
   const quotients = [];
   const remainders = [];
-
-  while (input > 0) {
-    const quotient = input / 2;
-    input = 0;
+  if (input === 0) {
+    result.innerText = "0";
+    return;
   }
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    result.innerText = remainders.reverse().join("");
+    input = quotient;
+  }
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
 };
 
 const checkUserInput = () => {
