bf4861bc13be26f1c5cefee6ff7851e6de232e43
AR123456 <coloradoz99@gmail.com>
2024-03-15 07:01:24 -0600

    setting up the app

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// get ID from URL -building an vanilla JS router
+console.log(window.location.pathname); //  index.html or whatever page the href is to
+// the current page
+const global = {
+  currentPage: window.location.pathname,
+};
+// Init App
+function init() {}
