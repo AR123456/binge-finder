bf0b5d751eb9753c2b8cfb6c4aac5fc5b535316e
AR123456 <coloradoz99@gmail.com>
2024-03-21 06:19:23 -0600

    shows

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const global = {
 // display (popular ) movies- add this to the DYI router
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
-  // console.log(results[0].backdrop_path.substring(1));
+
   results.forEach((movie) => {
     // display a card for each result
     const div = document.createElement("div");
@@ -36,23 +36,65 @@ async function displayPopularMovies() {
     <p class="card-text">
       <small class="text-muted">Release: ${movie.release_date}</small>
     </p>
- 
+
 </div>`;
     document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+
+  results.forEach((show) => {
+    // display a card for each result
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `  
+  <a href="tv-details.html?id=${show.id}">
+
+  ${
+    show.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.title}"
+  />`
+      : `    <img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.title}"
+/>`
+  }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${show.title}</h5>
+    <p class="card-text">
+      <small class="text-muted">Release: ${show.release_date}</small>
+    </p>
+
+</div>`;
+    document.querySelector("#popular-shows").appendChild(div);
   });
 }
 
 // Fetch data from TMDB API note   IRL this would be on the server
 async function fetchAPIData(endpoint) {
   const API_URL = "https://api.themoviedb.org/3/";
+  showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = await response.json();
+  hideSpinner();
   return data;
 }
-
+// show or hide spinner
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
 // highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
@@ -71,7 +113,7 @@ function init() {
       displayPopularMovies();
       break;
     case "/shows.html":
-      console.log("Show");
+      displayPopularShows();
       break;
     case "/search.html":
       console.log("search");
