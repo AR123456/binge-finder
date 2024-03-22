554cce5555e7d94461b8071ada9e8e38611ab873
AR123456 <coloradoz99@gmail.com>
2024-03-22 06:46:54 -0600

    movie detail start

--- js/script.js
+++ js/script.js
@@ -75,6 +75,58 @@ async function displayPopularShows() {
     document.querySelector("#popular-shows").appendChild(div);
   });
 }
+// display movie details
+async function displayMovieDetails() {
+  //?id=763215 split at = sign to get number get value a index 1
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  console.log(movie);
+  const div = document.createElement("div");
+  div.innerHTML = `   
+  <div class="details-top">
+    <div>
+      <img
+        src="${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />
+    </div>
+    <div>
+      <h2>Movie Title</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        8 / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+      
+        <li>${movie.genres[0].name}</li>
+        <li>${movie.genres[1].name}</li>
+        <li>${movie.genres[2].name}</li>
+     
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${movie.budget}</li>
+      <li><span class="text-secondary">Revenue:</span> $${movie.revenue}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${movie.production_companies[0].name}, ${movie.production_companies[1].name}</div>
+  
+</div>`;
+  document.querySelector("#movie-details").appendChild(div);
+}
 
 // Fetch data from TMDB API note   IRL this would be on the server
 async function fetchAPIData(endpoint) {
@@ -119,7 +171,7 @@ function init() {
       console.log("search");
       break;
     case "/movie-details.html":
-      console.log("movie d");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("tv d");
